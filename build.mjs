5c618b24781503f43a04fa4cbc03b85f990ebab9
AAAA <c326e738e52c5469f518071e@yahoo.com>
2021-03-05 17:09:39 -0600

    build: Fix the build script  - Use the correct input file name  - Create the `dist` folder if missing

--- build.mjs
+++ build.mjs
@@ -3,10 +3,12 @@ import sass from 'sass'
 import csso from 'csso'
 import fs from 'fs'
 
-const source = path.join('src', 'nushi.scss')
+const source = path.join('src', 'nushi.sass')
 const output = path.join('dist', 'nushi.min.css')
 
 const css = sass.renderSync({ file: source }).css
 const min = csso.minify(css, { restructure: false }).css
+
+if (!fs.existsSync('dist')) fs.mkdirSync('dist')
 
 fs.writeFileSync(output, min, { encoding: 'utf-8' })
