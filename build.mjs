3a5d03486691621eb2609b810a63aefef2fa52b0
AAAA <c326e738e52c5469f518071e@yahoo.com>
2021-05-07 22:16:24 -0500

    feat: Output source map

--- build.mjs
+++ build.mjs
@@ -3,12 +3,20 @@ import sass from 'sass'
 import csso from 'csso'
 import fs from 'fs'
 
-const source = path.join('src', 'nushi.sass')
-const output = path.join('dist', 'nushi.min.css')
+const file = path.join('src', 'nushi.sass')
+const outFile = path.join('dist', 'nushi.min.css')
 
-const css = sass.renderSync({ file: source }).css
-const min = csso.minify(css, { restructure: false }).css
+const res = sass.renderSync({ file, outFile, sourceMap: true })
+const css = csso.minify(res.css, { restructure: false }).css
+const map = (it => {
+  const cwd = process.cwd()
+  return it?.toString()
+    .replace('""', `"${cwd}"`)
+    .replaceAll(`file://${cwd}/`, '')
+})(res.map)
 
 if (!fs.existsSync('dist')) fs.mkdirSync('dist')
 
-fs.writeFileSync(output, min, { encoding: 'utf-8' })
+const cfg = { encoding: 'utf-8' }
+fs.writeFileSync(outFile, css, cfg)
+fs.writeFileSync(`${outFile}.map`, map, cfg)
